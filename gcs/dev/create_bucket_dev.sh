b62fa2ac50ddadc25657f3aba98044a8b0ad9586
gcpcloud <163750298+gcpcloudde@users.noreply.github.com>
2025-07-18 18:42:25 +0530

    Update create_bucket_dev.sh

--- gcs/dev/create_bucket_dev.sh
+++ gcs/dev/create_bucket_dev.sh
@@ -6,7 +6,7 @@ LOCATION="us-central1"
 STORAGE_CLASS="STANDARD"
 
 echo "Creating GCS bucket: $BUCKET_NAME in project: $PROJECT_ID"
-
+echo "new one"
 gcloud storage buckets create gs://$BUCKET_NAME \
   --project=$PROJECT_ID \
   --location=$LOCATION \
