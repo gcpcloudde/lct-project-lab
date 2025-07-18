c199d4cb50da62deeef26f3120b477a89bf074b6
gcpcloud <163750298+gcpcloudde@users.noreply.github.com>
2025-07-18 18:50:42 +0530

    Create create_new_gcs_bucket.sh

---
+++ gcs/dev/create_new_gcs_bucket.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_ID="lhn-dev-project"
+BUCKET_NAME="dummy-test-bucket2-$(date +%s)"
+LOCATION="us-central1"
+STORAGE_CLASS="STANDARD"
+
+echo "Creating GCS bucket: $BUCKET_NAME in project: $PROJECT_ID"
+echo "new one"
+gcloud storage buckets create gs://$BUCKET_NAME \
+  --project=$PROJECT_ID \
+  --location=$LOCATION \
+  --default-storage-class=$STORAGE_CLASS
